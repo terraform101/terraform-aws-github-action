f14d7a84a8eff70e7c9906840f3aeeb05e6c99a0
developer_jinsung <developer_jinsung@jinsung.site>
2024-01-08 12:24:17 +0900

    example

--- variables.tf
+++ variables.tf
@@ -36,3 +36,14 @@ variable "placeholder" {
   default     = "placekitten.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
+
+variable "environment" {
+  default     = "dev"
+  description = "Define infrastructure's environment"
+  type = string
+
+  validation {
+    condition     = var.environment == "dev" || var.environment == "qa" || var.environment == "prod"
+    error_message = "The environment value must be dev, qa or prod"
+  }
+}
