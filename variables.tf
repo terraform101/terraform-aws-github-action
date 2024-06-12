ebea62a88720b6a7a4781a3123be0957babde301
222 <02seo222@gmail.com>
2024-06-12 10:46:52 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -36,3 +36,9 @@ variable "placeholder" {
   default     = "placekitten.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
+
+variable "environment"{
+  default        =  "dev"
+  description    =  "Define infrastructure's environment"
+  type           =   string
+}
